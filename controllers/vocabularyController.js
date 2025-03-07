82e1794ebdda3d4907f9344f8927b42e38df2aff
phucnguyen09052003 <phucnguyen.090503@gmail.com>
2025-03-07 14:00:44 +0700

    SOLID SR + DIP

--- controllers/vocabularyController.js
+++ controllers/vocabularyController.js
@@ -1,110 +1,57 @@
-const sql = require('mssql');
+const vocabularyService = require("../services/VocabularyService");
 
 exports.getAllVocabulary = async (req, res) => {
     try {
-        const pool = await sql.connect();
-        const result = await pool.request().query('SELECT * FROM Vocabulary');
-        res.json(result.recordset);
+        const vocabularies = await vocabularyService.getAllVocabulary();
+        res.json(vocabularies);
     } catch (err) {
         res.status(500).send(err.message);
     }
 };
-
-exports.createWord = async (req, res) => {
-    const { Word, Translation, TopicID, Image } = req.body;
-
-    try {
-        const pool = await sql.connect();
-        const result = await pool.request()
-            .input('Word', sql.VarChar, Word)
-            .input('Translation', sql.NVarChar, Translation)
-            .input('TopicID', sql.VarChar, TopicID)
-            .input('Image', sql.VarChar, Image)
-            .query(`
-                INSERT INTO Vocabulary (Word, Translation, TopicID, Image) 
-                VALUES (@Word, @Translation, @TopicID, @Image)
-            `);
-
-        res.status(201).send(`Từ đã được thêm với ID: ${result.rowsAffected}`);
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
-};
-
-exports.updateWord = async (req, res) => {
-    const { Word, Translation, TopicID, Image } = req.body;
-    const id = req.params.id;
-
-    try {
-        const pool = await sql.connect();
-        const result = await pool.request()
-            .input('Word', sql.VarChar, Word)
-            .input('Translation', sql.NVarChar, Translation)
-            .input('TopicID', sql.NVarChar, TopicID)
-            .input('Image', sql.NVarChar, Image)
-            .input('ID', sql.Int, id)
-            .query(`
-                UPDATE Vocabulary 
-                SET Word = @Word, Translation = @Translation, TopicID = @TopicID, Image = @Image 
-                WHERE WordID = @ID
-            `);
-
-        if (result.rowsAffected[0] === 0) {
-            return res.status(404).send('Không tìm thấy từ để sửa.');
-        }
-
-        res.send('Từ đã được sửa thành công.');
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
-};
-
-exports.deleteWord = async (req, res) => {
-    const id = req.params.id; // Lấy ID từ tham số URL
-
-    try {
-        const pool = await sql.connect();
-        const result = await pool.request()
-            .input('ID', sql.Int, id) // Giả định ID là kiểu Int
-            .query(`
-        DELETE FROM Vocabulary 
-        WHERE WordID = @ID
-    `);
-
-        if (result.rowsAffected[0] === 0) {
-            return res.status(404).send('Không tìm thấy từ để xóa.');
-        }
-
-        res.send('Từ đã được xóa thành công.');
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
-};
-
-
-
 
 exports.getWordById = async (req, res) => {
-    const { id } = req.params;
     try {
-        const pool = await sql.connect();
-        const result = await pool.request()
-            .input('id', sql.Int, id)
-            .query('SELECT * FROM Vocabulary WHERE WordID = @id');
-        res.json(result.recordset[0]);
+        const word = await vocabularyService.getWordById(req.params.id);
+        if (!word) {
+            return res.status(404).send("Không tìm thấy từ.");
+        }
+        res.json(word);
     } catch (err) {
         res.status(500).send(err.message);
     }
 };
 
 exports.getVocabularyByTopic = async (req, res) => {
-    const { topicId } = req.params;
     try {
-        const pool = await sql.connect();
-        const result = await pool.request()
-            .input('TopicID', sql.Int, topicId)
-            .query('SELECT * FROM Vocabulary WHERE TopicID = @TopicID');
-        res.json(result.recordset);
+        const words = await vocabularyService.getVocabularyByTopic(req.params.topicId);
+        res.json(words);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+};
+
+exports.createWord = async (req, res) => {
+    try {
+        const message = await vocabularyService.createWord(req.body);
+        res.status(201).send(message);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+};
+
+exports.updateWord = async (req, res) => {
+    try {
+        const message = await vocabularyService.updateWord(req.params.id, req.body);
+        res.send(message);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+};
+
+exports.deleteWord = async (req, res) => {
+    try {
+        const message = await vocabularyService.deleteWord(req.params.id);
+        res.send(message);
     } catch (err) {
         res.status(500).send(err.message);
     }
--- routes/vocabulary.js
+++ routes/vocabulary.js
@@ -1,15 +1,66 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const vocabularyController = require('../controllers/vocabularyController');
+const vocabularyController = require("../controllers/vocabularyController");
 
-// Route lấy danh sách từ vựng
-router.get('', vocabularyController.getAllVocabulary);
+/**
+ * @swagger
+ * tags:
+ *   name: Vocabulary
+ *   description: API quản lý từ vựng
+ */
 
-// Route thêm từ vựng mới
-router.post('', vocabularyController.createWord);
-router.put('/:id', vocabularyController.updateWord);
-router.delete('/:id', vocabularyController.deleteWord);
-// Route lấy từ vựng theo TopicID
-router.get('/topic/:topicId', vocabularyController.getVocabularyByTopic);
+/**
+ * @swagger
+ * /api/vocabulary:
+ *   get:
+ *     summary: Lấy danh sách tất cả từ vựng
+ *     tags: [Vocabulary]
+ *     responses:
+ *       200:
+ *         description: Trả về danh sách từ vựng
+ */
+router.get("", vocabularyController.getAllVocabulary);
+
+/**
+ * @swagger
+ * /api/vocabulary/{id}:
+ *   get:
+ *     summary: Lấy từ vựng theo ID
+ *     tags: [Vocabulary]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID của từ vựng
+ *     responses:
+ *       200:
+ *         description: Trả về thông tin từ vựng
+ */
+router.get("/:id", vocabularyController.getWordById);
+
+/**
+ * @swagger
+ * /api/vocabulary/topic/{topicId}:
+ *   get:
+ *     summary: Lấy danh sách từ vựng theo chủ đề
+ *     tags: [Vocabulary]
+ *     parameters:
+ *       - in: path
+ *         name: topicId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID của chủ đề
+ *     responses:
+ *       200:
+ *         description: Trả về danh sách từ vựng theo chủ đề
+ */
+router.get("/topic/:topicId", vocabularyController.getVocabularyByTopic);
+
+router.post("", vocabularyController.createWord);
+router.put("/:id", vocabularyController.updateWord);
+router.delete("/:id", vocabularyController.deleteWord);
 
 module.exports = router;
