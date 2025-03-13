3aaaaaa82d8d9c4c78a4e946c1dbbddf37f29f4d
phucnguyen09052003 <phucnguyen.090503@gmail.com>
2025-03-13 11:27:40 +0700

    phuc add user

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,45 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+/**
+ * @swagger
+ * tags:
+ *   name: User
+ *   description: API quản lý User
+ */
 
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Lấy danh sách User
+ *     tags: [User]
+ *     responses:
+ *       200:
+ *         description: Trả về danh sách từ vựng
+ */
 router.get('', userController.getUser);
+
 router.post('', userController.createUser);
 router.post('/reset-password/:userId', userController.resetpassword);
+
+/**
+ * @swagger
+ * /api/users/{username}:
+ *   get:
+ *     summary: Lấy thông tin user theo username
+ *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         name: username
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Username
+ *     responses:
+ *       200:
+ *         description: Trả về thông tin user
+ */
 router.get('/:username', userController.getUserByName);
 router.delete('/:username', userController.deleteUser);
 
